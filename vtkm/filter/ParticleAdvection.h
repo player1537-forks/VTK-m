3380682d741eb5bbea2530ff26b107c24b4290e4
Dave Pugmire <dpugmire@gmail.com>
2021-11-11 09:21:15 -0500

    Filters for charged particle advection.

--- vtkm/filter/ParticleAdvection.h
+++ vtkm/filter/ParticleAdvection.h
@@ -23,7 +23,7 @@ namespace filter
 /// Takes as input a vector field and seed locations and generates the
 /// end points for each seed through the vector field.
 
-class ParticleAdvection : public vtkm::filter::FilterParticleAdvection<ParticleAdvection>
+class ParticleAdvection : public vtkm::filter::FilterParticleAdvection<ParticleAdvection, vtkm::Particle>
 {
 public:
   VTKM_CONT ParticleAdvection();
