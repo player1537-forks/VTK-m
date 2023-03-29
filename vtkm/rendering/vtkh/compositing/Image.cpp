cc4248c2e306a2463136e9da426434b434e9fa15
Dave Pugmire <dpugmire@gmail.com>
2023-03-29 15:20:02 -0400

    Add copyright statements...

--- vtkm/rendering/vtkh/compositing/Image.cpp
+++ vtkm/rendering/vtkh/compositing/Image.cpp
@@ -1,4 +1,12 @@
-// See License.txt
+//============================================================================
+//  Copyright (c) Kitware, Inc.
+//  All rights reserved.
+//  See LICENSE.txt for details.
+//
+//  This software is distributed WITHOUT ANY WARRANTY; without even
+//  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+//  PURPOSE.  See the above copyright notice for more information.
+//============================================================================
 
 #include <vtkm/rendering/vtkh/compositing/Image.hpp>
 //#include <png_utils/ascent_png_encoder.hpp>
